75bb2dd1bbebc9d1a825cfe1eea247bb7ada2d7e
Stephan Bösch-Plepelits <skunk@xover.htu.tuwien.ac.at>
2023-01-24 17:08:47 +0100

    Load existing users from Drupal; create new users

--- run.php
+++ run.php
@@ -1,8 +1,45 @@
 <?php
 $db = new PDO('mysql:dbname=fotokiste', 'skunk', 'PASSWORD');
+require 'lib/drupal-rest-php/drupal.php';
+$drupal = new DrupalRestAPI([
+  'url' => 'https://xover.mud.at/~tramway/fotokiste_neu',
+  'user' => 'plepelits@xover.mud.at',
+  'pass' => 'PASSWORD',
+  'authMethod' => 'cookie',
+]);
+
+
+$drupal_user = [];
+foreach ($drupal->loadRestExport('/rest/user', ['paginated' => true]) as $user) {
+  foreach ($user['mail'] as $mail) {
+    $drupal_user[$mail['value']] = $user['uid'][0]['value'];
+  }
+  foreach ($user['field_aliases'] as $mail) {
+    $drupal_user[$mail['value']] = $user['uid'][0]['value'];
+  }
+}
 
 function get_user_id ($sender) {
+  global $drupal_user;
+  global $drupal;
 
+  $sender = parse_sender($sender);
+
+  if (array_key_exists($sender['mail'], $drupal_user)) {
+    return $drupal_user[$sender['mail']];
+  }
+
+  $user = [
+    'name' => [['value' => $sender['name']]],
+    'field_name' => [['value' => $sender['name']]],
+    'mail' => [['value' => $sender['mail']]],
+    'status' => [['value' => false]],
+  ];
+
+  $user = $drupal->userSave(null, $user);
+
+  $drupal_user[$sender['mail']] = $user['uid'][0]['value'];
+  return $user['uid'][0]['value'];
 }
 
 function parse_sender ($sender) {
@@ -48,7 +85,7 @@ function parse_sender ($sender) {
   }
   elseif (preg_match('/^(.+) *<(.*)>$/', $sender, $m)) {
     return [
-      'name' => $m[1],
+      'name' => trim($m[1]),
       'mail' => $m[2],
     ];
   }
