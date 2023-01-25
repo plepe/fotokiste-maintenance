745030819ed3efe96696242d1d14bdf3e5eee367
Stephan Bösch-Plepelits <skunk@xover.htu.tuwien.ac.at>
2023-01-25 18:33:58 +0100

    Upload attachments

--- run.php
+++ run.php
@@ -104,9 +104,67 @@ function parse_sender ($sender) {
     ];
   }
 
-  print "$sender\n";
-
   return [];
+}
+
+function convert_body ($str) {
+  if (preg_match('/&[A-Za-z]+;/', $str)) {
+    return html_entity_decode($str);
+  }
+
+  return $str;
+}
+
+function convert_title ($str) {
+  $str = convert_body($str);
+
+  if (preg_match('/(.*)\[stadtverkehr-austria-fotos\](.*)$/', $str, $m)) {
+    return trim(trim($m[1]) . ' ' . trim($m[2]));
+  }
+
+  return $str;
+}
+
+function add_attachment (&$node, $elem) {
+  global $drupal;
+
+  $filepath = "/home/tramway/fotokiste_orig/attach/{$elem['kistenname']}/{$elem['msg_number']}_{$elem['att_id']}";
+
+  $file = $drupal->fileUpload([
+      'filename' => $elem['filename'],
+      'content' => file_get_contents($filepath)
+    ], 'media/photography/field_image'
+  );
+
+  $media = [
+    'bundle' => [['target_id' => 'photography']],
+    'name' => [['value' => $elem['filename']]],
+    'uid' => $node['uid'],
+    'created' => $node['created'],
+    'field_image' => [['target_id' => $file['fid'][0]['value']]],
+  ];
+
+  $media = $drupal->mediaSave(null, $media);
+
+  $node['field_attachments'][] = $media['mid'][0]['value'];
+}
+
+function update_all_files () {
+  global $drupal;
+
+  foreach ($drupal->loadRestExport('/rest/media', ['paginated' => true]) as $media) {
+    $fid = $media['field_image'][0]['target_id'];
+
+    $file = $drupal->fileGet($fid);
+
+    $fileUpdate = [
+      'uid' => $media['uid'],
+      'created' => $media['created'],
+      'type' => $file['type'],
+    ];
+
+    $drupal->fileSave($fid, $fileUpdate);
+  }
 }
 
 $res = $db->query('select sender from message group by sender');
@@ -116,6 +174,27 @@ if (!$res) {
 }
 
 while ($elem = $res->fetch()) {
-  print "* {$elem['sender']}: ";
-  print_r(parse_sender($elem['sender']));
+  print_r($elem);
+  $node = [
+    'type' => [['target_id' => 'message']],
+    'uid' => [['target_id' => get_user_id($elem['sender'])]],
+    'title' => [['value' => convert_title($elem['subject'])]],
+    'field_oldid' => [['value' => "{$elem['kistenname']}-{$elem['msg_number']}"]],
+    'created' => [['value' => $elem['date']]],
+    'body' => [['value' => convert_body($elem['body'])]],
+    'field_attachments' => [],
+  ];
+
+  $resA = $db->query("select * from attachment where kistenname=" . $db->quote($elem['kistenname']) . " and msg_number=" . $db->quote($elem['msg_number']));
+  while ($elemA = $resA->fetch()) {
+    add_attachment($node, $elemA);
+  }
+
+//  $update = [
+//    'type' => [['target_id' => 'message']],
+//    'field_attachments' => $node['field_attachments'],
+//  ];
+//  $node = $drupal->nodeSave(1, $update);
+
+  $node = $drupal->nodeSave(null, $node);
 }
